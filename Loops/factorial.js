06f69ef153bd288a95b97d6d84e98d64d73b570c
Mishra Atul <140804963+atul37936@users.noreply.github.com>
2026-02-24 20:06:09 +0530

    Refactor factorial calculation with improved logging

--- Loops/factorial.js
+++ Loops/factorial.js
@@ -12,7 +12,7 @@ console.log(`For loop :-Factorial using forward direction`)
 let a =1
 for(let b =1 ; b<=n; b++){
    a*=b
-   console.log(`Element of Sum ${b}`)
+   console.log(`Sum of Element ${b}`)
 }
 console.log(`The factorial of ${n} is ${a}`)
 
@@ -28,7 +28,7 @@ console.log(`For loop :-Factorial using Backward direction`)
 let c =1
 for(let b = n; b>=1; b--){
    c*=b
-   console.log(`Element of Sum ${b}`)
+   console.log(`Sum of Element ${b}`)
 }
 console.log(`The factorial of ${n} is ${c}`)
 
@@ -45,7 +45,7 @@ let d = 1
 let e = 1
 while (e<=n) {
    d*=e
-    console.log(`Element of Sum ${e}`)
+    console.log(`Sum of Element ${e}`)
     e++
 }
 console.log(`The factorial of ${n} is ${d}`)
@@ -63,7 +63,7 @@ let f = 1
 let g = n
 while (g>=1) {
   f*=g
-    console.log(`Element of Sum ${g}`)
+    console.log(`Sum of Element ${g}`)
     g--
 }
 console.log(`The factorial of ${n} is ${f}`)
@@ -81,7 +81,7 @@ let h = 1
 let i = 1
 do {
   h*=i
-    console.log(`Element of Sum ${i}`)
+    console.log(`Sum of Element ${i}`)
   i++
 } while (i<=n);
 console.log(`The factorial of ${n} is ${h}`)
@@ -101,7 +101,8 @@ let j = 1
 let k = n
 do {
   j*=k
-    console.log(`Element of Sum ${k}`)
+    console.log(`Sum of Element ${k}`)
   k--
 } while (k>=1);
+
 console.log(`The factorial of ${n} is ${j}`)
